f63784952c37c80a567d1a999cbd2a0333f46abe
DmitriNikonov <d.n.45@list.ru>
2023-06-09 12:33:19 +0600

    Initial commit

---
+++ app/src/main/java/me/smartlab/nikonov/Splash.java
@@ -0,0 +1,35 @@
+package me.smartlab.nikonov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import me.smartlab.nikonov.R;
+import me.smartlab.nikonov.Session;
+
+public class Splash extends AppCompatActivity
+{
+    private static final int SPLASH_SCREEN_TIMEOUT = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Intent intent = new Intent(Splash.this, Session.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, SPLASH_SCREEN_TIMEOUT * 1000);
+
+    }
+}
